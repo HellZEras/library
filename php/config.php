17dc5ce951ebf77ec18d98259a6038c945161d76
HellZEras <novaxz55@gmail.com>
2024-11-28 00:23:39 +0100

    session fix

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
 
 $timeout = 3600;
 ini_set('session.gc_maxlifetime', $timeout);
 ini_set('session.cookie_lifetime', $timeout);
+session_start();
 
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $timeout)) {
     unset($_SESSION['user_id']);
